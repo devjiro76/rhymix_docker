19c0501a21f825a53f542a1fcf306605cab615e8
Devjiro76 <devjiro76@gmail.com>
2017-07-05 10:56:27 +0900

    Update init.sh

--- init.sh
+++ init.sh
@@ -2,5 +2,5 @@ git clone --progress --verbose https://github.com/rhymix/rhymix.git "web"
 mkdir web/files
 chmod 777 web/files
 su -c "setenforce 0"
-docker-compose up -d
+docker-compose up -d --build
 docker-compose ps
